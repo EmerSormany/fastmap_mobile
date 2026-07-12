355501ecff0cbc70de10fb254c7b393a7e2e8e81
EmerSormany <sormanyemerson@gmail.com>
2026-07-12 16:50:20 -0300

    fix: correção de leitura de variáveis de ambiente no arquivo main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart'; 
-import 'core/constants/environment.dart';
 import 'core/models/fast_map_app.dart';
+import 'dart:convert'; // Para converter o JSON
+import 'package:flutter/services.dart';
 
 void main() async {
   // Garante que os componentes do Flutter carreguem antes do banco
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa o Supabase usando as variáveis do arquivo centralizado
+  // 1. Lê o arquivo config.json como texto
+  final configString = await rootBundle.loadString('config.json');
+  
+  // 2. Converte o texto para um Mapa (Objeto JSON)
+  final configData = jsonDecode(configString);
+
+  // 3. Inicializa o Supabase pegando os dados diretamente do JSON
   await Supabase.initialize(
-    url: Environment.supabaseUrl, 
-    anonKey: Environment.supabaseAnonKey,
+    url: configData['SUPABASE_URL'], 
+    anonKey: configData['SUPABASE_ANON_KEY'],
   );
 
   runApp(const FastMapApp());
